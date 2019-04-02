b9e0a4e7408e0c327490f94ef49a7eecf5a20faf
Aleksandr Ohotnikov <aleksohotnikov@gmail.com>
2019-04-02 23:12:06 +0400

    git flow example

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './components/App/app';
 
+// test git glow
 ReactDOM.render(<App name = "Joly"/>, document.querySelector('#root'))
